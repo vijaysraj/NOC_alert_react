293e1095a897df90f8dec1a48e96b6d5c11ef266
vijaysraj <vijaysraj006@gmail.com>
2017-11-07 10:39:39 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Pusher from 'pusher-js';
+// import  { setPusherClient } from 'react-pusher';
+// import Pusher from 'react-pusher';
+// import pusherClient from 'pusher-js';
+import logo from '../logo.svg';
+import '../App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      alert: ''
+    }
+    
+  }
+
+  componentDidMount() {
+    const pusher = new Pusher('f7fc554df3fe445f182f', {
+      cluster: 'ap2',
+      encrypted: true
+    });
+
+    const channel = pusher.subscribe('channel');
+    channel.bind('Notification', data => {
+      this.setState({alert: data.message})
+    }); 
+  }
+
+  render() {
+    return (
+      <div> {this.state.alert} </div>
+    );
+  }
+}
+
+export default App;
+
+
+
